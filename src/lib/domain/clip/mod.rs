4928ea60fcd7e15327077ad8903448b89095b06c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-08 04:17:07 +0530

    ...

--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -1,1 +1,13 @@
 pub mod field;
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Clip {
+    pub clip_id: field::ClipId,
+    pub short_code: field::ShortCode,
+    pub content: field::Content,
+    pub title: field::Title,
+    pub date: field::Posted,
+    pub expires: field::Expires,
+    pub password: field::Password,
+    pub hits: field::Hits,
+}

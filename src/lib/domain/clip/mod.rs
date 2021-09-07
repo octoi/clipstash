6ee5569a4c6846d205eeb03fafb22d7e38e60c4b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-08 04:46:14 +0530

    5

--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -1,5 +1,26 @@
 pub mod field;
 
+use serde::{ Serialize, Deserialize };
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ClipError {
+    #[error("invalid password: {0}")]
+    InvalidPassword(String),
+    #[error("invalid title: {0}")]
+    InvalidTitle(String),
+    #[error("empty content")]
+    EmptyContent,
+    #[error("invalid date: {0}")]
+    InvalidDate(String),
+    #[error("date parse error: {0}")]
+    DateParse(#[from] chrono::ParseError),
+    #[error("id parse error: {0}")]
+    Id(#[from] uuid::Error),
+    #[error("hits parse error: {0}")]
+    Hits(#[from] std::num::TryFromIntError),
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Clip {
     pub clip_id: field::ClipId,

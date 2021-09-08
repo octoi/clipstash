235ed6d8885d20d9b07cb9bb3f9e48a03062f764
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-08 12:15:19 +0530

    9

---
+++ src/lib/domain/clip/field/title.rs
@@ -0,0 +1,42 @@
+use super::ClipError;
+use crate::domain::clip::ClipError;
+use serde::{ Deserialize, Serialize };
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Title(Option<String>);
+
+impl Title {
+    pub fn new<T: Into<Option<String>>>(title: T) -> Self {
+        let title: Option<String>= title.into();
+
+        match title {
+            Some(title) => {
+                if !title.trim().is_empty() {
+                    Self(Some(title))
+                } else {
+                    Self(None)
+                }
+            },
+            None => Self(None)
+        }
+    }
+
+    pub fn into_inner(self) -> Option<String> {
+        self.0
+    }
+}
+
+impl Default for Title {
+    fn default() -> Self {
+        Self::new(None)
+    }
+}
+
+impl FromStr for Title {
+    type Err = ClipError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self::new(s.to_string()))
+    }
+}

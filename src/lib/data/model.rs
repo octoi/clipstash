8cb4016e93990e1f1b3160bec7b0319dff1037e7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-09 04:49:31 +0530

    25

--- src/lib/data/model.rs
+++ src/lib/data/model.rs
@@ -67,8 +67,22 @@ pub struct NewClip {
     pub(in crate::data) content: String,
     pub(in crate::data) title: Option<String>,
     pub(in crate::data) posted: i64,
-    pub(in crate::data) expires: Option<NaiveDateTime>,
+    pub(in crate::data) expires: Option<i64>,
     pub(in crate::data) password: Option<String>,
+}
+
+impl From<crate::service::ask::NewClip> for NewClip {
+    fn from(req: crate::service::ask::NewClip) -> Self {
+        Self {
+            clip_id: DbId::new().into(),
+            content: req.content.into_inner(),
+            title: req.title.into_inner(),
+            expires: req.expires.into_inner().map(|time| time.timestamp()),
+            password: req.password.into_inner(),
+            shortcode: ShortCode::default().into(),
+            posted: Utc::now().timestamp(),
+        }
+    }
 }
 
 pub struct UpdateClip {
@@ -77,4 +91,16 @@ pub struct UpdateClip {
     pub(in crate::data) title: Option<String>,
     pub(in crate::data) expires: Option<i64>,
     pub(in crate::data) password: Option<String>,
+}
+
+impl From<crate::service::ask::UpdateClip> for UpdateClip {
+    fn from(req: crate::service::ask::UpdateClip) -> Self {
+        Self {
+            content: req.content.into_inner(),
+            title: req.title.into_inner(),
+            expires: req.expires.into_inner().map(|time| time.timestamp()),
+            password: req.password.into_inner(),
+            shortcode: ShortCode::default().into(),
+        }
+    }
 }
--- src/lib/service/action.rs
+++ src/lib/service/action.rs
@@ -3,6 +3,14 @@ use crate::service::ask;
 use crate::{Clip, ShortCode, ServiceError};
 use std::convert::TryInto;
 
+pub async fn new_clip(req: ask::NewClip, pool: &DatabasePool) -> Result<Clip, ServiceError> {
+    Ok(query::new_clip(req, pool).await?.try_into()?)
+}
+
+pub async fn update_clip(req: ask::UpdateClip, pool: &DatabasePool) -> Result<Clip, ServiceError> {
+    Ok(query::update_clip(req, pool).await?.try_into()?)
+}
+
 pub async fn get_clip(req: ask::GetClip, pool: &DatabasePool) -> Result<Clip, ServiceError> {
     let user_password = req.password.clone();
     let clip: Clip = query::get_clip(req, &pool).await?.try_into()?;

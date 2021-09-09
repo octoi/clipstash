b75af56d4fc050dc3c2d1206959398ba055f8fc3
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-09 14:39:31 +0530

    27

---
+++ src/lib/web/ctx.rs
@@ -0,0 +1,69 @@
+use serde::Serialize;
+use derive_more::Constructor;
+
+pub trait PageContext {
+    fn title(&self) -> &str;
+    fn template_path(&self) -> &str;
+    fn parent(&self) -> &str;
+}
+
+#[derive(Debug, Serialize)]
+pub struct Home {}
+
+impl Default for Home {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl PageContext for Home {
+    fn title(&self) -> &str {
+        "Stash Your Clipboard"
+    }
+
+    fn template_path(&self) -> &str {
+        "home"
+    }
+
+    fn parent(&self) -> &str {
+        "base"
+    }
+}
+
+#[derive(Debug, Serialize, Constructor)]
+pub struct ViewClip {
+    pub clip: crate::Clip,
+}
+
+impl PageContext for ViewClip {
+    fn title(&self) -> &str {
+        "View Clip"
+    }
+
+    fn template_path(&self) -> &str {
+        "clip"
+    }
+
+    fn parent(&self) -> &str {
+        "base"
+    }
+}
+
+#[derive(Debug, Serialize, Constructor)]
+pub struct PasswordRequired {
+    shortcode: crate::ShortCode,
+}
+
+impl PageContext for PasswordRequired {
+    fn title(&self) -> &str {
+        "Password Required"
+    }
+
+    fn template_path(&self) -> &str {
+        "clip_need_password"
+    }
+
+    fn parent(&self) -> &str {
+        "base"
+    }
+}

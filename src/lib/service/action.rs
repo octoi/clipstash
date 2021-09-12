21fe95e10a139ca19e6afe6aac9067d096a28eb1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-12 06:12:59 +0530

    45

--- src/lib/service/action.rs
+++ src/lib/service/action.rs
@@ -2,6 +2,7 @@ use crate::data::{query, DatabasePool, Transaction};
 use crate::service::ask;
 use crate::{Clip, ShortCode, ServiceError};
 use std::convert::TryInto;
+use crate::web::api::ApiKey;
 
 pub async fn begin_transaction(pool: &DatabasePool) -> Result<Transaction<'_>, ServiceError> {
     Ok(pool.begin().await?)
@@ -40,4 +41,17 @@ pub async fn get_clip(req: ask::GetClip, pool: &DatabasePool) -> Result<Clip, Se
     } else {
         Ok(clip)
     }
+}
+
+pub async fn generate_api_key(pool: &DatabasePool) -> Result<ApiKey, ServiceError> {
+    let api_key = ApiKey::default();
+    Ok(query::save_api_key(api_key, pool).await?)
+}
+
+pub async fn revoke_api_key(api_key: ApiKey, pool: &DatabasePool) -> Result<query::RevocationStatus, ServiceError> {
+    Ok(query::revoke_api_key(api_key, pool).await?)
+}
+
+pub async fn api_key_is_valid(api_key: ApiKey, pool: &DatabasePool) -> Result<bool, ServiceError> {
+    Ok(query::api_key_is_valid(api_key, pool).await?)
 }

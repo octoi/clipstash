2d989fd95613cc4ccf6c6512ef4288691d66d63d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-12 06:30:13 +0530

    46

--- src/lib/web/api.rs
+++ src/lib/web/api.rs
@@ -10,6 +10,7 @@ use rocket::Responder;
 use rocket::State;
 use serde::Serialize;
 use std::str::FromStr;
+use crate::web::api::ApiError::KeyError;
 
 pub const API_KEY_HEADER: &str = "x-api-key";
 
@@ -79,7 +80,50 @@ impl From<ServiceError> for ApiError {
             ServiceError::NotFound => Self::NotFound(Json("entity not found".to_owned())),
             ServiceError::Data(_) => Self::Server(Json("a server error occurred".to_owned())),
             ServiceError::PermissionError(msg) => Self::User(Json(msg)),
+        }
+    }
+}
 
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey {
+    type Error = ApiError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        fn server_error() -> Outcome<ApiKey, ApiError> {
+            Outcome::Failure((
+                Status::InternalServerError,
+                ApiError::Server(Json("server error".to_string()))
+            ))
+        }
+
+        fn key_error(e: ApiKeyError) -> Outcome<ApiKey, ApiError> {
+            Outcome::Failure((
+                Status::BadRequest,
+                ApiError::KeyError(Json(e))
+            ))
+        }
+
+        match req.headers().get_one(API_KEY_HEADER) {
+            None => key_error(ApiKeyError::NotFound("Api key not found".to_string())),
+            Some(key) => {
+                let db = match req.guard::<&State<AppDatabase>>().await {
+                    Outcome::Success(db) => db,
+                    _ => return server_error(),
+                };
+
+                let api_key = match ApiKey::from_str(key) {
+                    Ok(key) => key,
+                    Err(e) => return key_error(e)
+                };
+
+                match action::api_key_is_valid(api_key.clone(), db.get_pool()).await {
+                    Ok(valid) if valid => Outcome::Success(api_key),
+                    Ok(valid) if !valid => {
+                        key_error(ApiKeyError::NotFound("API key not found".to_owned()))
+                    },
+                    _ => server_error()
+                }
+            }
         }
     }
 }
